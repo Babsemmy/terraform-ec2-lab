cc6ec7f62211a0f718c48e1b900bc345970d3e2e
Emmanuel <babatundeolaemma@gmail.comm>
2025-09-21 04:22:30 +0100

    Add static HTML CI/CD workflow

--- .github/workflows/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "<h1>Hello! Your CI/CD pipeline is working 🎉</h1>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
